ac4bd47162433a3d55e73886da08eaf31f90f8ad
Christian Campo <christian.campo@compeople.de>
2018-01-24 13:36:49 +0100

    update

--- prepare.bat
+++ prepare.bat
@@ -1,4 +1,4 @@
 set VSINSTALLDIR = C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\
-rem mkdir platforms
-rem mkdir platforms\windows
-rem mkdir platforms\windows\AppPackages
+mkdir platforms
+mkdir platforms\windows
+mkdir platforms\windows\AppPackages
